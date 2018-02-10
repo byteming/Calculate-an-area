75749175edd31e212c5c8674983e9e83f6690b91
qiuming1 <qmm0607@gmail.com>
2018-02-09 21:45:39 -0500

    Add files via upload

---
+++ CircleArea.java
@@ -0,0 +1,22 @@
+/*
+
+ Mingming Qiu
+ 
+ This program is to calculate the area of a circle.
+*/
+
+public class CircleArea{
+	
+	public static void main(String[] args){
+		
+		double r;
+		double area;
+		
+		r = 4.5;
+		area = 3.1425*r*r;
+		
+		System.out.println(" The area of a circle is "+ area);
+		
+	}//end main
+	
+}//end class
